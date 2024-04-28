e0d569ef87f052b1111d2041eef344da7b6bb328
Akitha Wijesinghe <akithawi@gmail.com>
2024-04-28 22:43:06 +0530

    ui improved

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
   final double bmi;
+  //final bool isMaleSelected;
 
   ResultPage({required this.bmi});
 
@@ -22,41 +23,49 @@ class ResultPage extends StatelessWidget {
       bmiStatusColor = Colors.orange;
     }
 
+    //const SizedBox(height: 100);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Result'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Your BMI is:',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              bmi.toStringAsFixed(2),
-              style: TextStyle(
-                fontSize: 36.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              bmiStatus,
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                color: bmiStatusColor,
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('BMI Result'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Image.asset(
+                'assets/female_image.png', // Add the path to the male image asset
+                width: 300,
+                height: 400,
+              ),
+              SizedBox(height: 20),
+              //SizedBox(height: 150),
+              Text(
+                'Your BMI is:',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                bmi.toStringAsFixed(2),
+                style: TextStyle(
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                bmiStatus,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: bmiStatusColor,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
